77f4d16e1115fcd55cf2b877acbc1e263635bb48
ixiLod <51421090+Dolix7@users.noreply.github.com>
2024-12-06 22:19:23 +0100

    consolide gltf models

--- src/components/Networks.tsx
+++ src/components/Networks.tsx
@@ -5,29 +5,11 @@ import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Html } from '@react-three/drei';
-import { Group } from 'three';
 
 interface Position {
   x: number;
   y: number;
   z: number;
-}
-
-interface ModelData {
-  url: string;
-  label: string;
-  position: [number, number, number];
-}
-
-interface ModelProps {
-  model: Group | null;
-  ref: React.MutableRefObject<Group | null>;
-  position: [number, number, number];
-  index: number;
-  label: string;
-  hoveredIndex: number | null;
-  setHoveredIndex: React.Dispatch<React.SetStateAction<number | null>>;
-  handleClick: (index: number) => void;
 }
 
 const quadraticBezier = (t: number, p0: Position, p1: Position, p2: Position): Position => {
@@ -37,85 +19,15 @@ const quadraticBezier = (t: number, p0: Position, p1: Position, p2: Position): P
   return { x, y, z };
 };
 
-const Model = ({
-  model,
-  ref,
-  position,
-  index,
-  label,
-  hoveredIndex,
-  setHoveredIndex,
-  handleClick,
-}: ModelProps) => (
-  <>
-    {model && (
-      <>
-        <primitive
-          ref={ref}
-          object={model}
-          scale={[0.2, 0.2, 0.2]}
-          position={position}
-          onPointerOver={() => {
-            setHoveredIndex(index);
-            document.body.style.cursor = 'pointer';
-          }}
-          onPointerOut={() => {
-            setHoveredIndex(null);
-            document.body.style.cursor = 'default';
-          }}
-          onClick={(e: React.MouseEvent<HTMLDivElement>) => {
-            e.stopPropagation();
-            handleClick(index);
-          }}
-        />
-        {hoveredIndex === index && (
-          <Html
-            position={[position[0], position[1] + 0.5, position[2]]}
-            style={{ pointerEvents: 'none' }}
-          >
-            <div
-              style={{
-                color: 'white',
-                background: 'rgba(255, 255, 255, 0.5)',
-                padding: '5px',
-                borderRadius: '5px',
-              }}
-            >
-              {label}
-            </div>
-          </Html>
-        )}
-      </>
-    )}
-  </>
-);
-
-const modelData: ModelData[] = [
-  {
-    url: '/assets/models/instagramLogo.gltf',
-    label: 'Instagram',
-    position: [0, -3, 1],
-  },
-  {
-    url: '/assets/models/linktreeLogo.gltf',
-    label: 'Linktree',
-    position: [0, -3, 1],
-  },
-  {
-    url: '/assets/models/githubLogo.gltf',
-    label: 'Github',
-    position: [0, -3, 1],
-  },
-];
-
 const Networks = () => {
-  const gltfRefs = [
-    useRef<THREE.Group | null>(null),
-    useRef<THREE.Group | null>(null),
-    useRef<THREE.Group | null>(null),
-  ];
+  const gltfRef1 = useRef<THREE.Group | null>(null);
+  const gltfRef2 = useRef<THREE.Group | null>(null);
+  const gltfRef3 = useRef<THREE.Group | null>(null);
+
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-  const [gltfModels, setGltfModels] = useState<(THREE.Group | null)[]>([null, null, null]);
+  const [gltfModel1, setGltfModel1] = useState<THREE.Group | null>(null);
+  const [gltfModel2, setGltfModel2] = useState<THREE.Group | null>(null);
+  const [gltfModel3, setGltfModel3] = useState<THREE.Group | null>(null);
 
   const positions: Position[] = [
     { x: -0.9, y: 1.1, z: 1 },
@@ -145,33 +57,38 @@ const Networks = () => {
   useEffect(() => {
     const loader = new GLTFLoader();
 
-    modelData.forEach((model, index) => {
+    const loadModel = (
+      url: string,
+      setModel: React.Dispatch<React.SetStateAction<THREE.Group | null>>,
+      ref: React.MutableRefObject<THREE.Group | null>,
+      modelName: string
+    ) => {
       loader.load(
-        model.url,
+        url,
         (gltf: GLTF) => {
-          setGltfModels((prev) => {
-            const newModels = [...prev];
-            newModels[index] = gltf.scene;
-            return newModels;
-          });
-          gltfRefs[index].current = gltf.scene;
+          setModel(gltf.scene);
+          ref.current = gltf.scene;
         },
         undefined,
         (error: unknown) => {
           if (error instanceof Error) {
-            console.error(`Error loading the model from ${model.url}:`, error);
+            console.error(`Error loading the ${modelName} model:`, error);
           } else {
-            console.error(`An unknown error occurred while loading the model from ${model.url}.`);
+            console.error(`An unknown error occurred while loading the ${modelName} model.`);
           }
         }
       );
-    });
+    };
+
+    loadModel('/assets/models/instagramLogo.gltf', setGltfModel1, gltfRef1, 'Instagram');
+    loadModel('/assets/models/linktreeLogo.gltf', setGltfModel2, gltfRef2, 'Linktree');
+    loadModel('/assets/models/githubLogo.gltf', setGltfModel3, gltfRef3, 'Github');
   }, []);
 
   useFrame((state) => {
     const elapsedTime = state.clock.getElapsedTime();
 
-    gltfRefs.forEach((ref, index) => {
+    [gltfRef1, gltfRef2, gltfRef3].forEach((ref, index) => {
       const targetPosition = positions[index];
       const startPosition = startPositions[index];
       const currentPosition = currentPositions.current[index];
@@ -226,19 +143,123 @@ const Networks = () => {
 
   return (
     <>
-      {modelData.map((model, index) => (
-        <Model
-          key={index}
-          model={gltfModels[index]}
-          ref={gltfRefs[index]}
-          position={model.position}
-          index={index}
-          label={model.label}
-          hoveredIndex={hoveredIndex}
-          setHoveredIndex={setHoveredIndex}
-          handleClick={handleClick}
-        />
-      ))}
+      {gltfModel1 && (
+        <>
+          <primitive
+            ref={gltfRef1}
+            object={gltfModel1}
+            scale={[0.2, 0.2, 0.2]}
+            position={[0, -3, 1]}
+            onPointerOver={() => {
+              setHoveredIndex(0);
+              document.body.style.cursor = 'pointer';
+            }}
+            onPointerOut={() => {
+              setHoveredIndex(null);
+              document.body.style.cursor = 'default';
+            }}
+            onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+              e.stopPropagation();
+              handleClick(0);
+            }}
+          />
+          {hoveredIndex === 0 && (
+            <Html
+              position={[positions[0].x, positions[0].y + 0.5, positions[0].z]}
+              style={{ pointerEvents: 'none' }}
+            >
+              <div
+                style={{
+                  color: 'white',
+                  background: 'rgba(255, 255, 255, 0.5)',
+                  padding: '5px',
+                  borderRadius: '5px',
+                }}
+              >
+                Instagram
+              </div>
+            </Html>
+          )}
+        </>
+      )}
+      {gltfModel2 && (
+        <>
+          <primitive
+            ref={gltfRef2}
+            object={gltfModel2}
+            scale={[0.2, 0.2, 0.2]}
+            position={[0, -3, 1]}
+            onPointerOver={() => {
+              setHoveredIndex(1);
+              document.body.style.cursor = 'pointer';
+            }}
+            onPointerOut={() => {
+              setHoveredIndex(null);
+              document.body.style.cursor = 'default';
+            }}
+            onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+              e.stopPropagation();
+              handleClick(1);
+            }}
+          />
+          {hoveredIndex === 1 && (
+            <Html
+              position={[positions[1].x, positions[1].y + 0.5, positions[1].z]}
+              style={{ pointerEvents: 'none' }}
+            >
+              <div
+                style={{
+                  color: 'white',
+                  background: 'rgba(255, 255, 255, 0.5)',
+                  padding: '5px',
+                  borderRadius: '5px',
+                }}
+              >
+                Linktree
+              </div>
+            </Html>
+          )}
+        </>
+      )}
+      {gltfModel3 && (
+        <>
+          <primitive
+            ref={gltfRef3}
+            object={gltfModel3}
+            scale={[0.2, 0.2, 0.2]}
+            position={[0, -3, 1]}
+            onPointerOver={() => {
+              setHoveredIndex(2);
+              document.body.style.cursor = 'pointer';
+            }}
+            onPointerOut={() => {
+              setHoveredIndex(null);
+              document.body.style.cursor = 'default';
+            }}
+            onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+              e.stopPropagation();
+              handleClick(2);
+            }}
+          />
+          {hoveredIndex === 2 && (
+            <Html
+              position={[positions[2].x, positions[2].y + 0.5, positions[2].z]}
+              style={{ pointerEvents: 'none' }}
+            >
+              <div
+                style={{
+                  color: 'white',
+                  background: 'rgba(255, 255, 255, 0.5)',
+                  padding: '5px',
+                  borderRadius: '5px',
+                }}
+              >
+                Github
+              </div>
+            </Html>
+          )}
+        </>
+      )}
     </>
   );
 };
