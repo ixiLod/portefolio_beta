2609d92be74fc1506e8730e826315b3247bf2430
ixiLod <51421090+Dolix7@users.noreply.github.com>
2024-12-17 02:28:07 +0100

    change next-sitemap config to js

--- next-sitemap.config.ts
+++
@@ -1,9 +0,0 @@
-import { IConfig } from 'next-sitemap';
-
-const config: IConfig = {
-  siteUrl: 'https://ixilod.com',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-};
-
-export default config;
