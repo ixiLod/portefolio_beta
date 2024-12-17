926fab5dcb785c25811ee56c515f97809c626be3
ixiLod <51421090+Dolix7@users.noreply.github.com>
2024-12-17 02:04:35 +0100

    test build sitemap + robots.txt

---
+++ next-sitemap.ts
@@ -0,0 +1,9 @@
+import { IConfig } from 'next-sitemap';
+
+const config: IConfig = {
+  siteUrl: 'https://ixilod.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
+
+export default config;
