595a2fff34ab65a4357350f74828878209712e5b
ixiLod <51421090+Dolix7@users.noreply.github.com>
2024-12-20 08:09:13 +0100

    exclude next/static for robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,12 @@ module.exports = {
   siteUrl: 'https://ixilod.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/ _next/static/'],
+      },
+    ],
+  },
 };
