58a44e53715841fbb42031df628c6ee4c21e2ffb
ixiLod <51421090+Dolix7@users.noreply.github.com>
2025-03-29 06:22:05 +0100

    add black bg opacity video

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -173,7 +173,7 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
             <source src="/creations/Disney.webm" type="video/webm" />
             Your browser does not support video playback.
           </video>
-          {/* <div className="absolute inset-0 bg-black bg-opacity-50"></div> */}
+          <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         </div>
 
         <div className="absolute inset-0 flex flex-col items-center justify-end p-8 text-center md:p-16">
